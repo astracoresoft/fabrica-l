94a95821a169b999a9b40365f056b8e03c208185
AntoxaHolovenko220 <antonio22022005@gmail.com>
2026-02-23 17:30:03 +0200

    complete team-block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import {
 	AboutBlock,
 	MetricBlock,
 	LocationsBlock,
+	TeamBlock,
 } from '@/components'
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
 			<AboutBlock />
 			<MetricBlock />
 			<LocationsBlock />
+			<TeamBlock />
 		</>
 	)
 }
